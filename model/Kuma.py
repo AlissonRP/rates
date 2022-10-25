37a8a094476e15b808a7575f73394391e91df7bf
AlissonRP <alirpereira887@gmail.com>
2022-10-25 12:31:33 -0300

    add plots, model and likelihood

--- model/Kuma.py
+++ model/Kuma.py
@@ -21,7 +21,7 @@ class Kuma:
 
     def pdf(self, x):
         """
-            x: valor a se calcular a densidade
+        x: valor a se calcular a densidade
         """
         if (np.all(0 < x) and np.all(x < 1)) == False:
             raise ValueError("X must be between 0 and 1")
@@ -39,7 +39,7 @@ class Kuma:
 
     def cumulative(self, x):
         if not 0 < x < 1:
-           raise ValueError("X must be between 0 and 1")
+            raise ValueError("X must be between 0 and 1")
         return 1 - (1 - (x**self.alpha)) ** self.beta
 
     def mean(self):
@@ -71,66 +71,73 @@ class Kuma:
             self.alpha = mle[0]
             self.beta = mle[1]
         return self
-    
+
     def vero(self, x):
         self.log_vero = -(
-                            (len(x) * np.log(self.alpha))
-                            + (len(x) * np.log(self.beta))
-                            + ((self.alpha - 1) * np.sum(np.log(x)))
-                            + ((self.beta - 1) * np.sum(np.log(1 - x**self.alpha)))
-                        )
+            (len(x) * np.log(self.alpha))
+            + (len(x) * np.log(self.beta))
+            + ((self.alpha - 1) * np.sum(np.log(x)))
+            + ((self.beta - 1) * np.sum(np.log(1 - x**self.alpha)))
+        )
         return self
-    
+
     def AIc(self):
-        self.AIC = 2*self.log_vero + 2*self.num_parameters
+        self.AIC = 2 * self.log_vero + 2 * self.num_parameters
         return self.AIC
 
     def BIc(self, x):
-        self.BIC = 2*self.log_vero + self.num_parameters*log(len(x))
+        self.BIC = 2 * self.log_vero + self.num_parameters * log(len(x))
         return self.BIC
 
     def AIcc(self, x):
-        self.AICC = 2*self.log_vero + 2*self.num_parameters*len(x)/(len(x) - self.num_parameters - 1)
+        self.AICC = 2 * self.log_vero + 2 * self.num_parameters * len(x) / (
+            len(x) - self.num_parameters - 1
+        )
         return self.AICC
 
-    def cramervonmises(self,x):
+    def cramervonmises(self, x):
         cdf = self.cumulative(np.sort(x))
         y = stats.norm.ppf(cdf)
-        u = stats.norm.cdf((y-np.mean(y))/np.std(y))
-        aux = [(element - (2*np.where(np.isclose(u, element))[0])/(2*len(x)))**2 for element in u]
-        w2 = sum(aux) + 1/(12*len(x))
-        self.cramermises = w2*(1+0.5/len(x))
-        return aux, w2,u
+        u = stats.norm.cdf((y - np.mean(y)) / np.std(y))
+        aux = [
+            (element - (2 * np.where(np.isclose(u, element))[0]) / (2 * len(x))) ** 2
+            for element in u
+        ]
+        w2 = sum(aux) + 1 / (12 * len(x))
+        self.cramermises = w2 * (1 + 0.5 / len(x))
+        return aux, w2, u
 
-    def plot(self, data):
+    def plot(self, data, legend_local="right"):
         """
         legend_local: local da legenda, pois a distribuicao pode ter ambas tipo de assimetria,
         entao pode ser necessario mover a legenda para esquerda
         """
-        ax = sns.histplot(data,kde=True, stat="density", bins = 20)
-        ax.lines[0].set_color('orange')
+        ax = sns.histplot(data, kde=True, stat="density", bins=20)
+        ax.lines[0].set_color("orange")
         x = np.random.uniform(0, 1, len(data))
-        ax = sns.kdeplot(teste.quantile(x))
+        ax = sns.kdeplot(self.quantile(x))
         plt.ylabel("Densidade")
-        plt.legend(title='Tipo', loc='upper right', labels=['Dados', 'Distribuição'])
+        plt.legend(
+            title="Tipo", loc="upper " + legend_local, labels=["Dados", "Distribuição"]
+        )
 
 
 #### TESTES  MANUAIS#####
-teste = Kuma(alpha=5, beta=3)
+# teste = Kuma(alpha=5, beta=3)
 
 
 #%%
-x = np.random.uniform(0, 1, 5000)
+# x = np.random.uniform(0, 1, 5000)
 
 
-sns.kdeplot(teste.quantile(x))
+# sns.kdeplot(teste.quantile(x))
 
 
 #%%
 
 
-x = np.random.uniform(0, 1, 5000)
-y = teste.quantile(x)
+# x = np.random.uniform(0, 1, 5000)
+# y = teste.quantile(x)
 
 
 #%
@@ -139,18 +146,14 @@ y = teste.quantile(x)
 
 
 #%%
-teste.fit([1, 2], y, change=True)
+# teste.fit([1, 2], y, change=True)
 
 #%%
 
 
-
-
-
-
-#sns.histplot(y)
+# sns.histplot(y)
 
 #%%
-teste.plot(y)
+# teste.plot(y)
 
 # %%
